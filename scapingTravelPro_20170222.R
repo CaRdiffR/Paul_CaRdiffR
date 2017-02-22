40713f8fcedf6ad9331da79a6ffae2518977ccfc
Paul Brennan <brennanpincardiff@gmail.com>
2017-02-22 08:09:53 +0000

    Initiating my web scraping project for CaRdiffR
    Trying to scrape vaccination data and disease risk data from
    travelhealthpro.org.uk

---
+++ scapingTravelPro_20170222.R
@@ -0,0 +1,14 @@
+# update 22 Feb 2017
+# my webscraping experiment for CaRdiffR, Cardiff R User Group
+
+# my plan is to try to scrape travel information 
+# the focus is on required vaccinations
+# and disease risk. 
+
+# one source of this data is http://travelhealthpro.org.uk/
+
+# list of countries: http://travelhealthpro.org.uk/countries
+
+# the countries are in an alphabetical list. 
+
+install.packages("rvest")
